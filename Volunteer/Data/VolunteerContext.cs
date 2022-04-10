7e96effca2a4030871ce62474652b713813a71c5
AleksandraRuzic <ruzic.aleksandra@gmail.com>
2022-04-10 21:45:31 +0200

    Created Data and Entity folder. Imported MongoDB.Driver

---
+++ Volunteer/Data/VolunteerContext.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+using Volunteer.Entities;
+
+namespace Volunteer.Data
+{
+    public class VolunteerContext : IVolunteerContext
+    {
+        public VolunteerContext()
+        {
+            var client = new MongoClient("mongo://localhost:27017");
+            var database = client.GetDatabase("Volunteer");
+            Volunteers = database.GetCollection<VolunteerInfo>("VolunteersInfo");
+        }
+
+        public IMongoCollection<VolunteerInfo> Volunteers { get; }
+    }
+}
---
+++ Volunteer/Entities/VolunteerInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Volunteer.Entities
+{
+    public class VolunteerInfo
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Array Skills { get; set; }
+    }
+}
