ffe0e3e54614791b1af85f6c3fadc6273db0a1c8
Vbatocanin <vladimir@wings.rs>
2022-04-14 21:36:55 +0200

    Fixed pipeline react test command and fixed test

--- volunteer-hub-frontend/src/App.test.tsx
+++ volunteer-hub-frontend/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders basic landing page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Home/i);
   expect(linkElement).toBeInTheDocument();
 });
