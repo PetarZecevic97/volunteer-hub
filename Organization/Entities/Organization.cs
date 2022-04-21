547dace1859b9e106a42b2337ed7d77731e87cd9
Nikola Peric <nikola.peric303@gmail.com>
2022-04-21 20:59:35 +0200

    Added a seeder for Organization DB

--- Organization/Entities/Organization.cs
+++ Organization/Entities/Organization.cs
@@ -9,5 +9,14 @@ namespace Organization.Entities
         public string OrganizationId { get; private set; }
         public DateTime JoinedOn { get; set; }
         public string Description { get; set; }
+
+        public Organization(int id, string organizationName, string organizationId, DateTime joinedOn, string description)
+        {
+            Id = id;
+            OrganizationName = organizationName ?? throw new ArgumentNullException(nameof(organizationName));
+            OrganizationId = organizationId ?? throw new ArgumentNullException(nameof(organizationId));
+            JoinedOn = joinedOn;
+            Description = description ?? throw new ArgumentNullException(nameof(description));
+        }
     }
 }
---
+++ Organization/Persistence/OrganizationContextSeed.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Organization.Persistence
+{
+    public class OrganizationContextSeed
+    {
+        public static async Task SeedAsync(OrganizationContext organizationContext, ILogger<OrganizationContextSeed> logger) {
+
+            if (!organizationContext.Organizations.Any()) 
+            {
+                organizationContext.Organizations.AddRange(GetPredefinedOrganizations());
+                await organizationContext.SaveChangesAsync();
+                logger.LogInformation("Seeding database associated with context {DbContextName}", nameof(organizationContext));
+            }
+        
+        }
+
+        private static IEnumerable<Entities.Organization> GetPredefinedOrganizations()
+        {
+            var organization1 = new Entities.Organization(1, "organization_1", "asd1234", DateTime.MinValue, "This is a very cool organization!");
+            var organization2 = new Entities.Organization(2, "organization_2", "ghjk8890", DateTime.MinValue, "We love dogs, come help us pet them!");
+            var organization3 = new Entities.Organization(3, "organization_3", "xyz987", DateTime.MinValue, "The government is lying to you, help us spread the word!");
+            return new List<Entities.Organization> { organization1, organization2, organization3 };
+        }
+    }
+}
