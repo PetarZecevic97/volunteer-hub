3e78669f26234dec6a5df66283cd79102090ef1d
Nikola Peric <nikola.peric303@gmail.com>
2022-04-21 19:55:03 +0200

    Implemented DB Context

--- Organization/Entities/Organization.cs
+++ Organization/Entities/Organization.cs
@@ -5,9 +5,9 @@ namespace Organization.Entities
     public class Organization
     {
         public int Id { get; private set; }
-        public string OrganizationName { get; private set; }
+        public string OrganizationName { get; set; }
         public string OrganizationId { get; private set; }
-        public DateTime JoinedOn { get; private set; }
-        public string Description { get; private set; }
+        public DateTime JoinedOn { get; set; }
+        public string Description { get; set; }
     }
 }
---
+++ Organization/Persistence/OrganizationContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Organization.Persistence
+{
+    public class OrganizationContext : DbContext
+    {
+        public OrganizationContext(DbContextOptions options) : base(options)
+        {
+        }
+        
+        public DbSet<Entities.Organization> Organizations { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            //For evry new organization in datebase we set JoinedOn property to be current time
+            foreach (var entity in ChangeTracker.Entries<Entities.Organization>())
+            {
+                switch (entity.State) {
+                    case EntityState.Added:
+                        entity.Entity.JoinedOn = DateTime.Now;
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Adding configuration defined for Organization tabel
+            modelBuilder.ApplyConfiguration(new EntityConfigurations.OrganizationEntityTypeConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
