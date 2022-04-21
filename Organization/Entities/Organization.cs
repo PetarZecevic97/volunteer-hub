466f07dfe8952d34f4646cd099d91379533bd94d
Nikola Peric <nikola.peric303@gmail.com>
2022-04-21 17:53:02 +0200

    Organization Entity
    Added Organization Entity and EFC configuration for it.

---
+++ Organization/Entities/Organization.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Organization.Entities
+{
+    public class Organization
+    {
+        public int Id { get; private set; }
+        public string OrganizationName { get; private set; }
+        public string OrganizationId { get; private set; }
+        public DateTime JoinedOn { get; private set; }
+        public string Description { get; private set; }
+    }
+}
---
+++ Organization/Persistence/EntityConfigurations/OrganizationEntityTypeConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace Organization.Persistence.EntityConfigurations
+{
+    public class OrganizationEntityTypeConfiguration : IEntityTypeConfiguration<Entities.Organization>
+    {
+        public void Configure(EntityTypeBuilder<Entities.Organization> builder)
+        {
+            builder.ToTable("Organization");
+            builder.HasKey(o => o.Id);
+            builder.Property(o => o.Id).UseHiLo("organizationseq");
+            
+            builder.Property<string>("OrganizationName")
+                .HasColumnType("VARCHAR(30)")
+                .HasColumnName("OrganizationName")
+                .IsRequired();
+
+            builder.Property<string>("OrganizationId")
+                .HasColumnType("VARCHAR(30)")
+                .HasColumnName("OrganizationId");
+
+            builder.Property<DateTime>("JoinedOn")
+                .HasColumnType("datetime")
+                .HasColumnName("JoinedOn");
+
+            builder.Property<string>("Description")
+                .HasColumnType("VARCHAR(100)")
+                .HasColumnName("Description");            
+        }
+    }
+}
