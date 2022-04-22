b1f9c2a4b428c37710cc34f0f63535288bad98e6
Nikola Peric <nikola.peric303@gmail.com>
2022-04-22 22:15:29 +0200

    Implemented organization controller
    - simple controller with one HttpGet method to get all organization from DB

---
+++ Organization/Controllers/OrganizationController.cs
@@ -0,0 +1,33 @@
+﻿using Organization.Repositories.Interfaces;
+using Organization.Entities;
+using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Organization.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class OrganizationController : ControllerBase
+    {
+
+        private readonly IAsyncRepository<OrganizationEntity> _organizationRepository;
+
+        public OrganizationController(IAsyncRepository<OrganizationEntity> organizationRepository)
+        {
+            _organizationRepository = organizationRepository ?? throw new ArgumentNullException(nameof(organizationRepository));
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<OrganizationEntity>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<OrganizationEntity>>> GetOrganizations()
+        {
+            var organizations = await _organizationRepository.GetAllAsync();
+            return Ok(organizations);
+        }
+
+    }
+}
