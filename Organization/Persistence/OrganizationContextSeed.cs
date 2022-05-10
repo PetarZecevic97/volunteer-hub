5e384e3cc794a00fab3a63d7b60cb2275c6a6350
Nikola Peric <nikola.peric303@gmail.com>
2022-05-10 22:52:30 +0200

    Implemented Organization DB Migration
    Seeding Organization DB with predefined data in case it is empty.

--- Organization/Persistence/OrganizationContextSeed.cs
+++ Organization/Persistence/OrganizationContextSeed.cs
@@ -11,6 +11,9 @@ namespace Organization.Persistence
     {
         public static async Task SeedAsync(OrganizationContext organizationContext, ILogger<OrganizationContextSeed> logger) {
 
+            /* NOTE: If we add more data through code (that is, through seeding function "GetPredefinedOrganizations()") their insertion will
+             *       be skipped because organizationContext.Organizations won't be empty.                  
+             */
             if (!organizationContext.Organizations.Any()) 
             {
                 organizationContext.Organizations.AddRange(GetPredefinedOrganizations());
@@ -22,10 +25,13 @@ namespace Organization.Persistence
 
         private static IEnumerable<OrganizationEntity> GetPredefinedOrganizations()
         {
-            var organization1 = new OrganizationEntity(1, "nikola", DateTime.MinValue, "...", DateTime.MinValue, "organization_1", "asd1234", "This is a very cool organization!");
-            var organization2 = new OrganizationEntity(2, "nikola", DateTime.MinValue, "...", DateTime.MinValue, "organization_2", "ghjk8890", "We love dogs, come help us pet them!");
-            var organization3 = new OrganizationEntity(3, "nikola", DateTime.MinValue, "...", DateTime.MinValue, "organization_3", "xyz987", "The government is lying to you, help us spread the word!");
-            return new List<OrganizationEntity> { organization1, organization2, organization3 };
+            var organization1 = new OrganizationEntity(1, "nikola", DateTime.Now, "...", DateTime.Now, "organization_1", "asd1234", "This is a very cool organization!");
+            var organization2 = new OrganizationEntity(2, "nikola", DateTime.Now, "...", DateTime.Now, "organization_2", "ghjk8890", "We love dogs, come help us pet them!");
+            var organization3 = new OrganizationEntity(3, "nikola", DateTime.Now, "...", DateTime.Now, "organization_3", "xyz987", "The government is lying to you, help us spread the word!");
+            var organization4 = new OrganizationEntity(4, "nikola", DateTime.Now, "...", DateTime.Now, "organization_4", "www333", "We have a really cool description here!");
+            var organization5 = new OrganizationEntity(5, "nikola", DateTime.Now, "...", DateTime.Now, "organization_5", "bnm789", "Yet another description!");
+
+            return new List<OrganizationEntity> { organization1, organization2, organization3, organization4, organization5 };
         }
     }
 }
