d20ebc33eef3cc6e9374a5b2589f98de926596fa
Nikola Peric <nikola.peric303@gmail.com>
2022-05-12 17:14:06 +0200

    Organization entity enhancement
    Changed OrganizationEntity constructor to accept only information relevant for organization

--- Organization/Entities/EntityBase.cs
+++ Organization/Entities/EntityBase.cs
@@ -14,13 +14,5 @@ namespace Organization.Entities
 
         public DateTime LastModifiedDate { get; set; }
 
-        public EntityBase(int id, string createdBy, DateTime createdDate, string lastModifiedBy, DateTime lastModifiedDate)
-        {
-            Id = id;
-            CreatedBy = createdBy ?? throw new ArgumentNullException(nameof(createdBy));
-            CreatedDate = createdDate;
-            LastModifiedBy = lastModifiedBy ?? throw new ArgumentNullException(nameof(lastModifiedBy));
-            LastModifiedDate = lastModifiedDate;
-        }
     }
 }
