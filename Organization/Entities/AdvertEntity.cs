b3ac566c42e32943be3b64914dfa6c7d4019900e
Nikola Peric <nikola.peric303@gmail.com>
2022-05-16 22:15:20 +0200

    Implemented Advert entity

---
+++ Organization/Entities/AdvertEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Organization.Entities
+{
+    public class AdvertEntity : EntityBase
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string EventLocation { get; set; }
+        public DateTime EventDate { get; set; }
+
+        public AdvertEntity(string title, string description, string eventLocation, DateTime eventDate)
+        {
+            Title = title ?? throw new ArgumentNullException(nameof(title));
+            Description = description ?? throw new ArgumentNullException(nameof(description));
+            EventLocation = eventLocation ?? throw new ArgumentNullException(nameof(eventLocation));
+            EventDate = eventDate;
+        }
+    }
+}
--- Organization/Entities/OrganizationEntity.cs
+++ Organization/Entities/OrganizationEntity.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Organization.Entities
 {
@@ -8,11 +9,21 @@ namespace Organization.Entities
         public string OrganizationId { get; private set; }
         public string Summary { get; set; }
 
+        private readonly List<AdvertEntity> _adverts = new List<AdvertEntity>();
+        public IReadOnlyCollection<AdvertEntity> Adverts => _adverts;
+
         public OrganizationEntity(string organizationName, string organizationId, string summary)            
         {
             OrganizationName = organizationName ?? throw new ArgumentNullException(nameof(organizationName));
             OrganizationId = organizationId ?? throw new ArgumentNullException(nameof(organizationId));
             Summary = summary ?? throw new ArgumentNullException(nameof(summary));
         }
+
+        public void AddAdvert(string title, string description, string eventLocation, DateTime eventDate) 
+        {             
+            var advert = new AdvertEntity(title, description, eventLocation, eventDate);
+            _adverts.Add(advert);
+        }
+        
     }
 }
