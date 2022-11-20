a6459e057d56f7774766122341f144a1a05ec3bf
Petar Zecevic <petar.b.zecevic@gmail.com>
2022-11-20 21:21:51 +0100

    Add identity server

---
+++ IdentityServer/Controllers/AuthenticationController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using AutoMapper;
+using IdentityServer.Entities;
+using System.Threading.Tasks;
+using IdentityServer.DTOs;
+using Microsoft.AspNetCore.Http;
+using IdentityServer.Controllers.Base;
+using IdentityServer.Services;
+
+namespace IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController: RegistrationControllerBase
+    {
+        private readonly IAuthenticationService _authService;
+        public AuthenticationController(ILogger<AuthenticationController> logger, IMapper mapper, UserManager<User> userManager, RoleManager<IdentityRole> roleManager, IAuthenticationService authService)
+            : base(logger, mapper, userManager, roleManager)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("[action]")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> RegisterUser([FromBody] NewUserDto newUser)
+        {
+            return await RegisterNewUserWithRoles(newUser, new string[] { "User" });
+
+        }
+
+        [HttpPost("[action]")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> RegisterAdministrator([FromBody] NewUserDto newUser)
+        {
+            return await RegisterNewUserWithRoles(newUser, new string[] { "Administrator" });
+        }
+
+        [HttpPost("[action]")]
+        [ProducesResponseType(typeof(AuthenticationModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> Login([FromBody] UserCredentialsDto userCredentials)
+        {
+            var user = await _authService.ValidateUser(userCredentials);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(await _authService.CreateAuthenticationModel(user));
+        }
+    }
+}
---
+++ IdentityServer/Extensions/IdentityExtensions.cs
@@ -0,0 +1,88 @@
+﻿using IdentityServer.Data;
+using IdentityServer.Entities;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Session;
+using System.Reflection;
+using AutoMapper;
+using System;
+using Microsoft.AspNetCore.Builder;
+using IdentityServer.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace IdentityServer.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static IServiceCollection ConfigurePersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IdentityContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("IdentityConnectionString"));
+            });
+            return services;
+        }
+
+        public static IServiceCollection ConfigureIdentity(this IServiceCollection services)
+        {
+            services.AddIdentity<User, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 8;
+                options.User.RequireUniqueEmail = true;
+            })
+                .AddEntityFrameworkStores<IdentityContext>()
+                .AddDefaultTokenProviders();
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureMiscellaneousServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+            return services;
+
+        }
+
+        public static IServiceCollection ConfigureJWT(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("JwtSettings");
+            var secretKey = jwtSettings.GetSection("secretKey").Value;
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+
+                        ValidIssuer = jwtSettings.GetSection("validIssuer").Value,
+                        ValidAudience = jwtSettings.GetSection("validAudience").Value,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+                    };
+                });
+
+            return services;
+
+        }
+    }
+
+}
