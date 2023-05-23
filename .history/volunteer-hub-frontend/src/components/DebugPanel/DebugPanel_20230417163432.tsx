05bbc606c4460f1ad3ff2911044a3dd3041b68f3
Vladimir Batoćanin <vladimir@wings.rs>
2023-05-23 17:32:26 +0200

    Delete .history directory

--- .history/volunteer-hub-frontend/src/components/DebugPanel/DebugPanel_20230417163432.tsx
+++
@@ -1,7 +0,0 @@
-import react from 'react';
-
-const DebugPanel = () => {
-
-};
-
-export default DebugPanel;
--- .history/volunteer-hub-frontend/src/components/Sidenav/Sidenav_20230417161426.tsx
+++
@@ -1,5 +0,0 @@
-import React, { useState, useEffect } from "react";
-
-const Sidenav = () => {
-
-}
