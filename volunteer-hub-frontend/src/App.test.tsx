8c9fd862c5fa83d1345e56f9fadc9624ae9aedfc
Vbatocanin <vladimir@wings.rs>
2023-05-24 18:49:22 +0200

    Reformatted react test

--- volunteer-hub-frontend/src/App.test.tsx
+++ volunteer-hub-frontend/src/App.test.tsx
@@ -4,13 +4,16 @@ import App from './App';
 import { Provider } from "react-redux";
 import configureStore from "./store";
 
-test('renders basic landing page', () => {
-  render(
-    <Provider store={configureStore()}>
-      <App />
-    </Provider>
-  );
-  
-  const linkElement = screen.getByText(/Home/i);
-  expect(linkElement).toBeInTheDocument();
+describe("React app", () => {
+  test('renders basic landing page', () => {
+    render(
+        <Provider store={configureStore()}>
+          <App />
+        </Provider>
+    );
+
+    const linkElement = screen.getByText("Home");
+    expect(linkElement).toBeInTheDocument();
+  });
 });
+
