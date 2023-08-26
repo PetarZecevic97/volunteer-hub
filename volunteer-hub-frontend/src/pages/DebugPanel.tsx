948f760d77eae64ec69059e8207caf61c1506542
Vladimir Batoćanin <vladimir@wings.rs>
2023-08-27 00:22:42 +0200

    Added MUI DebugPanel

--- volunteer-hub-frontend/src/pages/DebugPanel.tsx
+++ volunteer-hub-frontend/src/pages/DebugPanel.tsx
@@ -1,134 +1,193 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import {
-    DebugTitle,
-    DebugForm,
-    DebugError,
-    DebugSubmit,
-    DebugContainer,
-    ButtonWrapper,
-    Form,
-    CheckBoxContainer,
-} from "../components/DebugPanel/styles/DebugPanelSC";
-import {WebRequestsInterface} from "../webRequests/webRequests-int";
-import getWebRequest from "../webRequests/webRequestsProvider";
-import {useNavigate} from "react-router-dom";
-import "../components/DebugPanel/styles/DebugPanel.css";
-import {CheckBox} from "../components/DebugPanel/CheckBox";
-import {TextInput} from "../components/DebugPanel/TextInput";
-import {checkIsLoggedIn, getUserInfo, setDebugValue, setUserInfo} from "../utility/Services/SessionService";
+  Box,
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+  Grid,
+  Paper,
+  RadioGroup,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import {
+  getUserInfo,
+  setDebugValue,
+  setUserInfo,
+} from "../utility/Services/SessionService";
 
 interface IErrorMessages {
-    name?: string;
-    email?: string;
-    password?: string;
-    message?: string;
+  name?: string;
+  email?: string;
+  password?: string;
+  message?: string;
 }
 
+const useStyles = makeStyles((theme) => ({
+  debugContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    backgroundColor: "#171616",
+  },
+  debugForm: {
+    padding: theme.spacing(3),
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: "#7600ff",
+  },
+  debugTitle: {
+    fontSize: "25px",
+    marginBottom: theme.spacing(2),
+  },
+  debugInputContainer: {
+    marginBottom: theme.spacing(2),
+  },
+  debugSubmit: {
+    width: "100%",
+    padding: theme.spacing(1.5),
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: "#ffbb00",
+    color: "#000000",
+    fontSize: "18px",
+    textTransform: "none",
+    "&:hover": {
+      backgroundColor: "#7600ff",
+    },
+  },
+  debugError: {
+    color: "red",
+    fontSize: "12px",
+  },
+  checkboxGroup: {
+    flexDirection: "column",
+    gap: theme.spacing(1),
+  },
+  checkbox: {
+    color: theme.palette.secondary.main, // Default color for checkboxes
+    '&.Mui-checked': {
+      color: theme.palette.secondary.main, // Amber color for checked checkboxes
+    },
+  },
+}));
+
 const DebugPanel = () => {
-    const [errorMessages, setErrorMessages] = useState<IErrorMessages>();
+  const classes = useStyles();
+  const [errorMessages, setErrorMessages] = useState<IErrorMessages>();
 
-    const [flags, setFlags] = useState([
-        {id: "debug_admin_toggle", label: "Admin", checked: true},
-        {id: "debug_jwt_toggle", label: "Require JWT Token", checked: true},
-        {id: "debug_all_visibility_toggle", label: "All content visible", checked: true},
-        {id: "debug_auth_toggle", label: "Authorization checks", checked: true},
-        {id: "debug_volunteer_toggle", label: "Volunteer account", checked: true},
-        {id: "debug_organization_toggle", label: "Organization account", checked: true},
-    ]);
+  const [flags, setFlags] = useState([
+    { id: "debug_admin_toggle", label: "Admin", checked: true },
+    { id: "debug_jwt_toggle", label: "Require JWT Token", checked: true },
+    { id: "debug_all_visibility_toggle", label: "All content visible", checked: true },
+    { id: "debug_auth_toggle", label: "Authorization checks", checked: true },
+    { id: "debug_volunteer_toggle", label: "Volunteer account", checked: true },
+    { id: "debug_organization_toggle", label: "Organization account", checked: true },
+  ]);
 
+  const [sessionInfo, setSessionInfo] = useState(getUserInfo());
+  const navigate = useNavigate();
 
-    const [sessionInfo, setSessionInfo] = useState(getUserInfo());
-    const navigate = useNavigate();
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    setDebugValues(event.currentTarget);
+  };
 
-    const userService: WebRequestsInterface = getWebRequest();
+  const handleChange = (isChecked: boolean, i: number) => {
+    let tmp = flags[i];
+    tmp.checked = !isChecked;
+    let flagsClone = [...flags];
+    flagsClone[i] = tmp;
+    setFlags([...flagsClone]);
+  };
 
-    const handleSubmit = (event: any) => {
-        setDebugValues(event);
-    };
+  const setDebugValues = (form: any) => {
+    const username = form.username.value;
+    const email = form.email.value;
 
-    const handleChange = (isChecked, i) => {
-        let tmp = flags[i];
-        tmp.checked = !isChecked;
-        let flagsClone = [...flags];
-        flagsClone[i] = tmp;
-        setFlags([...flagsClone]);
-        console.log(flags)
-    };
+    setUserInfo(username, email);
 
-    const setDebugValues = (event: any) => {
-        //Prevent page reload
-        event.preventDefault();
+    flags.map(({ id, label, checked }, i) => {
+      setDebugValue(checked, label);
+    });
+  };
 
-        const username = event.currentTarget.username.value;
-        const email = event.currentTarget.email.value;
-
-        setUserInfo(username, email);
-        console.log(getUserInfo())
-
-        flags.map(({id, label, checked}, i) => {
-            setDebugValue(checked,label)
-        });
-    };
-
-    // Generate JSX code for error message
-    const renderErrorMessage = (name: string) => {
-        if (errorMessages) {
-            return (
-                name === errorMessages.name && (
-                    <DebugError>{errorMessages.message}</DebugError>
-                )
-            );
-        }
-    };
-
-    // Generate JSX code for Debug form
-    const renderForm = (
-        <Form onSubmit={handleSubmit}>
-            <TextInput
-                p_label={"Username"}
-                p_name={"username"}
-                p_isRequired={true}
-                p_renderErrorMessage={renderErrorMessage}/>
-            <TextInput
-                p_label={"Email"}
-                p_name={"email"}
-                p_isRequired={true}
-                p_renderErrorMessage={renderErrorMessage}/>
-
-            {/* Select type of account */}
-            Account flags:
-            <CheckBoxContainer>
-                {flags.map(({id, label, checked}, i) => (
-                    <CheckBox
-                        p_id={id}
-                        p_label={label}
-                        p_checked={checked}
-                        p_index={i}
-                        p_handle_change={handleChange}
-                    />
-                ))}
-
-
-            </CheckBoxContainer>
-
-            <ButtonWrapper>
-                <DebugSubmit type="submit" value="Update Info"/>
-            </ButtonWrapper>
-        </Form>
-    );
-    if (checkIsLoggedIn()) {
-        navigate("/profile", {replace: true});
-        return <></>;
-    } else {
-        return (
-            <DebugContainer>
-                <DebugForm>
-                    <DebugTitle>Manually set user info</DebugTitle>
-                    {renderForm}
-                </DebugForm>
-            </DebugContainer>
-        );
+  const renderErrorMessage = (name: string) => {
+    if (errorMessages) {
+      return (
+        name === errorMessages.name && (
+          <FormHelperText className={classes.debugError}>
+            {errorMessages.message}
+          </FormHelperText>
+        )
+      );
     }
+  };
+
+  return (
+    <Box className={classes.debugContainer}>
+      <Paper className={classes.debugForm}>
+        <Typography className={classes.debugTitle}>
+          Manually set user info
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={2} className={classes.debugInputContainer}>
+            <Grid item xs={12} md={6}>
+              <TextField
+                fullWidth
+                label="Username"
+                name="username"
+                required
+                variant="outlined"
+              />
+              {renderErrorMessage("username")}
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                fullWidth
+                label="Email"
+                name="email"
+                required
+                variant="outlined"
+              />
+              {renderErrorMessage("email")}
+            </Grid>
+          </Grid>
+          <FormControl component="fieldset" className={classes.debugInputContainer}>
+            <Typography variant="subtitle1">Account Flags:</Typography>
+            <RadioGroup
+              className={classes.checkboxGroup}
+              aria-label="account-flags"
+            >
+              {flags.map(({ id, label, checked }, i) => (
+                <FormControlLabel
+                  key={id}
+                  control={
+                    <Checkbox
+                      className={classes.checkbox}
+                      checked={checked}
+                      onChange={() => handleChange(checked, i)}
+                    />
+                  }
+                  label={label}
+                />
+              ))}
+            </RadioGroup>
+          </FormControl>
+          <Button
+            type="submit"
+            variant="contained"
+            className={classes.debugSubmit}
+          >
+            Update Info
+          </Button>
+        </form>
+      </Paper>
+    </Box>
+  );
 };
 
 export default DebugPanel;
