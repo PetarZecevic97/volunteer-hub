b86a4380dae49f487723ae762dc92371719f3df0
Vladimir Batoćanin <vladimir@wings.rs>
2023-09-15 23:44:18 +0200

    Fixed issue where profiles wouldn't display fully

--- volunteer-hub-frontend/src/pages/Organization.tsx
+++ volunteer-hub-frontend/src/pages/Organization.tsx
@@ -1,40 +1,77 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect,  } from "react";
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import Avatar from "react-avatar";
-import { Grid, PageContainer } from "../components/Profile/styles/ProfileSC";
-import { useSelector } from 'react-redux';
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import { useSelector, connect } from "react-redux";
 import { getOrganization } from "../actions/organizationActions";
-import { connect } from "react-redux";
 
 const Organization = ({ getOrganizationAction }: any) => {
-  const myProfileOrganization = useSelector((state: any) => state.profileData.myProfile);
-  const currentOrganization = useSelector((state: any) => state.organizations.organization);
+  const myProfileOrganization = useSelector(
+    (state: any) => state.profileData.myProfile
+  );
+  const currentOrganization = useSelector(
+    (state: any) => state.organizations.organization
+  );
   const { organizationId } = useParams();
 
   function chooseRightOrg() {
     return organizationId ? currentOrganization : myProfileOrganization;
   }
-  const [rightOrganization, setRightOrganization] = useState(chooseRightOrg());
+  const [rightOrganization, setRightOrganization] = useState(
+    chooseRightOrg()
+  );
 
   useEffect(() => {
-    if(organizationId && !currentOrganization) {
+    if (organizationId && !currentOrganization) {
       getOrganizationAction(organizationId);
     }
     setRightOrganization(chooseRightOrg());
   }, [organizationId, currentOrganization, myProfileOrganization]);
 
   return (
-    <>
-      <PageContainer>
-        <Grid>
-          <Avatar size="50" round={true} name={rightOrganization ? rightOrganization.organizationName : ""} />
-
-          <h1>Organization name: {rightOrganization ? rightOrganization.organizationName : ""}</h1>
-          <p>Summary: {rightOrganization ? rightOrganization.summary : ""}</p>
-        </Grid>
-      </PageContainer>
-    </>
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <Paper
+          elevation={3}
+          style={{
+            padding: "16px",
+            backgroundColor: "#8c41ff", // Light purple background color
+          }}
+        >
+          <Grid container spacing={2} justifyContent="center">
+            <Grid item>
+              <Avatar
+                size="100"
+                round={true}
+                name={
+                  rightOrganization
+                    ? rightOrganization.organizationName
+                    : "Organization"
+                }
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h4" component="div">
+                {rightOrganization
+                  ? rightOrganization.organizationName
+                  : "Organization Name"}
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="body1">
+                Summary:{" "}
+                {rightOrganization
+                  ? rightOrganization.summary
+                  : "No Summary Available"}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Grid>
+    </Grid>
   );
 };
 
